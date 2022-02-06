ceff0961f9eff750a22199b5c68f0696152ef051
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-02-06 09:35:15 +0530

    init

---
+++ android/app/src/main/kotlin/com/praveenvarma/personal_diary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.praveenvarma.personal_diary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyDiary(),
+    );
+  }
+}
+
+class MyDiary extends StatelessWidget {
+  const MyDiary({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
