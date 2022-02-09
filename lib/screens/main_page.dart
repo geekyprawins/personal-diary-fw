1f743ca6fb4bfce73a7b03508e429c5389b7adcd
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-02-10 01:43:57 +0530

    long time

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -64,10 +65,116 @@ class _MainPageState extends State<MainPage> {
                     }
                   },
                 ),
+              ),
+              Row(
+                children: [
+                  Column(
+                    children: const [
+                      Expanded(
+                        child: InkWell(
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundImage:
+                                  NetworkImage("https://picsum.photos/200/300"),
+                              backgroundColor: Colors.transparent,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Praveen",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.login,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
               )
             ],
-          )
+          ),
         ],
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border(
+                  right: BorderSide(
+                    width: 0.4,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ),
+              child: ListView(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SfDateRangePicker(
+                    onSelectionChanged: (daterangepickerSelection) {
+                      // TODO: add functionality
+                    },
+                  ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.greenAccent,
+                      size: 40,
+                    ),
+                    label: const Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Write New',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.white,
+              child: ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: Card(
+                      elevation: 4,
+                      child: ListTile(
+                        title: Text("Hello, $index"),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: 5,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: "Add",
+        child: const Icon(
+          Icons.add,
+        ),
       ),
     );
   }
